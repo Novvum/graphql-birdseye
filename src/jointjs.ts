86ed0f15f1820a9b6efabf0a65acac29c5d939f8
Rohit <rohit@novvuminc.com>
2018-12-10 11:57:04 -0800

    positioning and routing

--- src/jointjs.ts
+++ src/jointjs.ts
@@ -1,4 +1,5 @@
 import joint from "jointjs/index";
+var _ = require("lodash");
 
 joint.shapes.basic.Generic.define(
   "devs.Model",
@@ -6,7 +7,7 @@ joint.shapes.basic.Generic.define(
     inPorts: [],
     outPorts: [],
     size: {
-      width: 200,
+      width: 300,
       height: "auto"
     },
     attrs: {
@@ -275,3 +276,159 @@ joint.dia.Link.define(
 );
 
 export default joint;
+
+export function adjustVertices(graph: any, cell: any) {
+  // if `cell` is a view, find its model
+  cell = cell.model || cell;
+
+  if (cell instanceof joint.dia.Element) {
+    // `cell` is an element
+
+    _.chain(graph.getConnectedLinks(cell))
+      .groupBy(function(link: any) {
+        // the key of the group is the model id of the link's source or target
+        // cell id is omitted
+        return _.omit([link.source().id, link.target().id], cell.id)[0];
+      })
+      .each(function(group: any, key: any) {
+        // if the member of the group has both source and target model
+        // then adjust vertices
+        if (key !== "undefined") adjustVertices(graph, _.first(group));
+      })
+      .value();
+
+    return;
+  }
+
+  // `cell` is a link
+  // get its source and target model IDs
+  var sourceId = cell.get("source").id || cell.previous("source").id;
+  var targetId = cell.get("target").id || cell.previous("target").id;
+
+  // if one of the ends is not a model
+  // (if the link is pinned to paper at a point)
+  // the link is interpreted as having no siblings
+  if (!sourceId || !targetId) return;
+
+  // identify link siblings
+  var siblings = _.filter(graph.getLinks(), function(sibling: any) {
+    var siblingSourceId = sibling.source().id;
+    var siblingTargetId = sibling.target().id;
+
+    // if source and target are the same
+    // or if source and target are reversed
+    return (
+      (siblingSourceId === sourceId && siblingTargetId === targetId) ||
+      (siblingSourceId === targetId && siblingTargetId === sourceId)
+    );
+  });
+
+  var numSiblings = siblings.length;
+  switch (numSiblings) {
+    case 0: {
+      // the link has no siblings
+      break;
+    }
+    case 1: {
+      // there is only one link
+      // no vertices needed
+      cell.unset("vertices");
+      break;
+    }
+    default: {
+      // there are multiple siblings
+      // we need to create vertices
+
+      // find the middle point of the link
+      var sourceCenter = graph
+        .getCell(sourceId)
+        .getBBox()
+        .center();
+      var targetCenter = graph
+        .getCell(targetId)
+        .getBBox()
+        .center();
+      var midPoint = joint.g.Line(sourceCenter, targetCenter).midpoint();
+
+      // find the angle of the link
+      var theta = sourceCenter.theta(targetCenter);
+
+      // constant
+      // the maximum distance between two sibling links
+      var GAP = 20;
+
+      _.each(siblings, function(sibling: any, index: any) {
+        // we want offset values to be calculated as 0, 20, 20, 40, 40, 60, 60 ...
+        var offset = GAP * Math.ceil(index / 2);
+
+        // place the vertices at points which are `offset` pixels perpendicularly away
+        // from the first link
+        //
+        // as index goes up, alternate left and right
+        //
+        //  ^  odd indices
+        //  |
+        //  |---->  index 0 sibling - centerline (between source and target centers)
+        //  |
+        //  v  even indices
+        var sign = index % 2 ? 1 : -1;
+
+        // to assure symmetry, if there is an even number of siblings
+        // shift all vertices leftward perpendicularly away from the centerline
+        if (numSiblings % 2 === 0) {
+          offset -= (GAP / 2) * sign;
+        }
+
+        // make reverse links count the same as non-reverse
+        var reverse = theta < 180 ? 1 : -1;
+
+        // we found the vertex
+        var angle = joint.g.toRad(theta + sign * reverse * 90);
+        var vertex = joint.g.Point.fromPolar(offset, angle, midPoint);
+
+        // replace vertices array with `vertex`
+        sibling.vertices([vertex]);
+      });
+    }
+  }
+}
+
+export function addTools(paper: any, link: any) {
+  var toolsView = new joint.dia.ToolsView({
+    tools: [
+      new joint.linkTools.SourceArrowhead(),
+      new joint.linkTools.TargetArrowhead()
+    ]
+  });
+  link.findView(paper).addTools(toolsView);
+}
+
+export function bindInteractionEvents(
+  adjustVertices: any,
+  graph: any,
+  paper: any
+) {
+  // bind `graph` to the `adjustVertices` function
+  var adjustGraphVertices = _.partial(adjustVertices, graph);
+
+  // adjust vertices when a cell is removed or its source/target was changed
+  graph.on("add remove change:source change:target", adjustGraphVertices);
+
+  // adjust vertices when the user stops interacting with an element
+  paper.on("cell:pointerup", adjustGraphVertices);
+}
+
+export function bindToolEvents(paper: any) {
+  // show link tools
+  paper.on("link:mouseover", function(linkView: any) {
+    linkView.showTools();
+  });
+
+  // hide link tools
+  paper.on("link:mouseout", function(linkView: any) {
+    linkView.hideTools();
+  });
+  paper.on("blank:mouseover cell:mouseover", function() {
+    paper.hideTools();
+  });
+}
