6f0cd63f9af9e0fb1ae27bd3bac8556fca0710bc
Rohit <rohit@novvuminc.com>
2019-01-22 11:22:28 -0800

    Revert "upgraded packages"
    This reverts commit ebea512cda2a70eeb357e6a82c39880d589b8bd3.

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import GraphqlBirdseye from "graphql-birdseye";
-import { bigSchema /** smallSchema */ as dummySchema } from "./dummySchema";
+import { smallSchema /** bigSchema */ as dummySchema } from "./dummySchema";
 export default class App extends Component {
   render() {
     return (
--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,27 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import visualizer from "rollup-plugin-visualizer";
-
+import typescript from 'rollup-plugin-typescript2'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
 // import postcss from 'rollup-plugin-postcss-modules'
-import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
-import url from "rollup-plugin-url";
-import svgr from "@svgr/rollup";
+import postcss from 'rollup-plugin-postcss'
+import resolve from 'rollup-plugin-node-resolve'
+import url from 'rollup-plugin-url'
+import svgr from '@svgr/rollup'
 
-import pkg from "./package.json";
+import pkg from './package.json'
 
 export default {
-  input: "src/index.tsx",
+  input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
-      format: "cjs",
-      exports: "named",
+      format: 'cjs',
+      exports: 'named',
       sourcemap: true
     },
     {
       file: pkg.module,
-      format: "es",
-      exports: "named",
+      format: 'es',
+      exports: 'named',
       sourcemap: true
     }
   ],
@@ -39,10 +37,6 @@ export default {
       rollupCommonJSResolveHack: true,
       clean: true
     }),
-    commonjs(),
-    visualizer({
-      filename: "./dist/statistics.html",
-      title: "GraphQL Birdseye"
-    })
+    commonjs()
   ]
-};
+}
