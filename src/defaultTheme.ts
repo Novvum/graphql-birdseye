b6caaebda5089d40219cf94e174e99ae0c5bb47b
Rohit <rohit@novvuminc.com>
2019-03-25 13:08:26 -0700

    moved theme type definition into own file

--- src/defaultTheme.ts
+++ src/defaultTheme.ts
@@ -1,3 +1,5 @@
+import { Row, Rect, Theme } from "./Theme";
+
 const colors = {
   primary: "#548f9e",
   background: "#212C32",
@@ -64,57 +66,4 @@ export default {
   }
 } as Theme;
 
-export interface Theme {
-  gap: number;
-  colors: Colors;
-  header: Header;
-  container: Rect;
-  divider: Rect;
-  row: Row;
-  line: Line;
-}
-
-interface Rect {
-  [key: string]: any;
-}
-
-interface Text {
-  [key: string]: any;
-}
-
-interface Line {
-  [key: string]: any;
-  targetMarker: TargetMarker;
-}
-
-interface TargetMarker {
-  [key: string]: any;
-  type: string;
-  d: string;
-}
-
-interface Row {
-  [key: string]: any;
-  height: number;
-  fieldNameLabel: Text;
-  fieldTypeLabel: Text;
-  body: Rect;
-}
-
-interface Header {
-  height: number;
-  label: Text;
-  container: Rect;
-}
-
-export interface Colors {
-  primary: string;
-  background: string;
-  white: string;
-  line: LineColors;
-}
-
-interface LineColors {
-  active: string;
-  inactive: string;
-}
+export * from './Theme';
