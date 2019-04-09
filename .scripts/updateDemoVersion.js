8012bf0379ac4ed36b271c78af5c846ae64f3f7b
Rohit <rohit@novvuminc.com>
2019-04-09 14:57:55 -0700

    logging which version of birdseye

--- .scripts/updateDemoVersion.js
+++ .scripts/updateDemoVersion.js
@@ -3,6 +3,7 @@ const path = require("path");
 const birdseyePackage = require("../package.json");
 const demoPackage = require("../example/package.json");
 
+console.log(`Setting graphql-birdseye version to ${birdseyePackage.version}`);
 demoPackage.dependencies["graphql-birdseye"] = birdseyePackage.version;
 
 fs.writeFileSync(
