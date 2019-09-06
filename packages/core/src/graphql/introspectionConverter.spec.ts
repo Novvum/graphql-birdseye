71c7990c4736ebffaf1733a8af881ee759103ccc
Rohit <rohit@novvuminc.com>
2019-09-05 22:47:32 -0700

    added interfaces and unions to data structure

--- packages/core/src/graphql/introspectionConverter.spec.ts
+++ packages/core/src/graphql/introspectionConverter.spec.ts
@@ -1,53 +1,68 @@
-import IntrospectionBirdseye from './introspectionConverter';
-import { BirdseyeDataStructure, Type } from '../dataStructure';
-import dummySchema from './dummySchema';
+import IntrospectionBirdseye from "./introspectionConverter";
+import { BirdseyeDataStructure, Type } from "../dataStructure";
+import dummySchema from "./dummySchema.new";
 
-describe('IntrospectionConverter', () => {
+describe("IntrospectionConverter", () => {
   const schema = dummySchema.data.__schema as any;
   let birdseye;
   beforeEach(() => {
     birdseye = new IntrospectionBirdseye(schema);
-  })
-  it('inherits from Birdseye datastructure', () => {
-    expect(IntrospectionBirdseye.prototype).toBeInstanceOf(BirdseyeDataStructure)
-  })
-  it('initializes properly', () => {
+  });
+  it("inherits from Birdseye datastructure", () => {
+    expect(IntrospectionBirdseye.prototype).toBeInstanceOf(
+      BirdseyeDataStructure
+    );
+  });
+  it("initializes properly", () => {
     expect(birdseye).toBeTruthy();
-  })
-  it('creates a typeMap', () => {
-    expect(birdseye.typeMap).toBeTruthy()
-  })
-  describe('Types', () => {
+  });
+  it("creates a typeMap", () => {
+    expect(birdseye.typeMap).toBeTruthy();
+  });
+  describe("Types", () => {
     let type;
     beforeEach(() => {
-      type = new IntrospectionBirdseye(schema).typeMap.Comic;
-    })
-    it('has fields', () => {
+      type = new IntrospectionBirdseye(schema).typeMap.Post;
+    });
+    it("has fields", () => {
       expect(type.fieldMap).toBeTruthy();
-    })
+    });
     describe("Fields", () => {
       let fields;
       beforeEach(() => {
-        fields = new IntrospectionBirdseye(schema).typeMap.Comic.fieldMap;
-      })
-      it('is defined', () => {
+        fields = new IntrospectionBirdseye(schema).typeMap.Post.fieldMap;
+      });
+      it("is defined", () => {
         expect(fields.id).toBeTruthy();
-      })
-      it('can be another type', () => {
-        expect(fields.variants.type).toBeInstanceOf(Type);
-      })
-      it('can be ID type', () => {
+      });
+      it("can be a type", () => {
+        expect(fields.author.type).toBeInstanceOf(Type);
+      });
+      it("can be ID", () => {
         expect(fields.id.type).toBe("ID");
-      })
-      it('can be String type', () => {
+      });
+      it("can be String", () => {
         expect(fields.title.type).toBe("String");
-      })
-      it('has a name and typeLabel', () => {
-        const variants = fields.variants;
-        expect(variants.name).toBe("variants");
-        expect(variants.typeLabel).toBe("[Summary!]");
-      })
-    })
-  })
-})
-
+      });
+      it("can be a List", () => {
+        expect(fields.relatedPosts.type).toBeInstanceOf(Type);
+        expect(fields.relatedPosts.typeLabel).toBe("[Post!]!");
+      });
+      it("has a name and typeLabel", () => {
+        const author = fields.author;
+        expect(author.name).toBe("author");
+        expect(author.typeLabel).toBe("User");
+      });
+    });
+  });
+  describe("Interfaces", () => {
+    let type;
+    beforeEach(() => {
+      type = new IntrospectionBirdseye(schema).typeMap.Publication;
+    });
+    it("has fields", () => {
+      expect(type.fieldMap).toBeTruthy();
+    });
+  });
+  describe("Unions", () => {});
+});
